348f7988d2ff5b9567c8caf8eb32df8ae3c29db0
Grant Miller <gmiller@extron.com>
2020-01-30 13:04:36 -0500

    init commit changed SimpleTCP into its own package

---
+++ simple_tcp/tcp_client.py
@@ -0,0 +1,168 @@
+import socket
+import time
+from threading import Timer
+
+
+class SimpleTCPClient:
+    def __init__(self, hostname, ipport, autoConnect=True):
+        self._hostname = hostname
+        self._ipport = ipport
+        self._autoConnect = autoConnect
+
+        self._sock = None
+
+        self._onConnectedCallback = None
+        self._onDisconnectedCallback = None
+        self._connectionStatus = None
+
+        self._onReceiveCallback = None
+        self._timerParseReceive = None
+
+        self._timerAutoConnect = None
+
+        if self._autoConnect:
+            self.Connect(1)
+
+    def onConnected(self, callback):
+        self._onConnectedCallback = callback
+        if 'Connected' in self._connectionStatus:
+            self._onConnectedCallback(self, self._connectionStatus)
+        return callback
+
+    def onDisconnected(self, callback):
+        self._onDisconnectedCallback = callback
+        if 'Disconnected' in self._connectionStatus:
+            self._onDisconnectedCallback(self, self._connectionStatus)
+        return callback
+
+    def onReceive(self, callback):
+        self._onReceiveCallback = callback
+        return callback
+
+    def _NewConnectionStatus(self, newState):
+        if newState != self._connectionStatus:
+            self._connectionStatus = newState
+            if 'Connected' in self._connectionStatus:
+                if self._onConnectedCallback:
+                    self._onConnectedCallback(self, 'Connected')
+
+                self._RestartReceiveLoop()
+
+            elif 'Disconnected' in self._connectionStatus:
+                if self._onDisconnectedCallback:
+                    self._onDisconnectedCallback(self, 'Disconnected')
+
+                self._StopReceiveLoop()
+
+        if self._autoConnect and 'Disconnected' in self._connectionStatus:
+            if self._timerAutoConnect and self._timerAutoConnect.isAlive():
+                self._timerAutoConnect.cancel()
+            self._timerAutoConnect = Timer(5, self.Connect)
+            self._timerAutoConnect.start()
+
+    def _RestartReceiveLoop(self):
+        if self._timerParseReceive is None:
+            self._timerParseReceive = Timer(0.5, self._ParseReceiveData)
+            self._timerParseReceive.start()
+
+        elif self._timerParseReceive.isAlive():
+            self._timerParseReceive.cancel()
+            self._timerParseReceive = None
+            self._RestartReceiveLoop()
+        else:
+            self._timerParseReceive = None
+            self._RestartReceiveLoop()
+
+    def _StopReceiveLoop(self):
+        if self._timerParseReceive and self._timerParseReceive.isAlive():
+            self._timerParseReceive.cancel()
+        self._timerParseReceive = None
+
+    def _ParseReceiveData(self):
+        # print('ParseReceiveData')
+        try:
+            rxData = self._sock.recv(1024)
+            if rxData == b'':
+                self.Disconnect()
+            if self._onReceiveCallback:
+                self._onReceiveCallback(self, rxData)
+        except Exception as e:
+            if 'timed out' in str(e):
+                pass
+
+        if 'Connected' in self._connectionStatus:
+            self._RestartReceiveLoop()
+
+    def Connect(self, timeout=None):
+        print('Connect(timeout=', timeout)
+        try:
+            if self._sock is None:
+                self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._sock.settimeout(timeout or 10)
+            self._sock.connect((self._hostname, self._ipport))
+            self._NewConnectionStatus('Connected')
+        except Exception as e:
+            print('Connection Error:', e)
+            if 'A connect request was made on an already connected socket' in str(e):
+                self._NewConnectionStatus('Connected')
+                return 'Connected'
+
+            self._NewConnectionStatus('Disconnected:' + str(e))
+
+        return self._connectionStatus
+
+    @property
+    def ConnectionStatus(self):
+        return self._connectionStatus
+
+    def Disconnect(self):
+        if 'Disconnected' in self._connectionStatus:
+            return self._connectionStatus
+
+        try:
+            if self._sock is not None:
+                self._sock.shutdown(1)
+                self._sock.close()
+        except Exception as e:
+            pass
+
+        self._NewConnectionStatus('Disconnected')
+        self._sock = None
+        return self._connectionStatus
+
+    def Send(self, data):
+        if isinstance(data, str):
+            data = data.encode(encoding='iso-8859-1')
+
+        try:
+            self._sock.send(data)
+        except:
+            try:
+                self.Disconnect()
+            except:
+                pass
+
+
+if __name__ == '__main__':
+    import random
+
+    # client = SimpleTCPClient('192.168.68.119', 3888)
+    client = SimpleTCPClient('10.8.27.171', 23)
+
+
+    @client.onConnected
+    @client.onDisconnected
+    @client.onReceive
+    def Event(_, data):
+        if data == 'Connected':
+            client.Send('\x1b3cv\r')
+        print('Event:', data)
+
+
+    while True:
+        cmd = random.choice(['q', 'n', 'i', '\x1bcc\r'])
+        # client.Send('The time is {}'.format(time.asctime()))
+        print('sending:', cmd)
+        if 'Connected' in client.ConnectionStatus:
+            client.Send(cmd)
+        time.sleep(5)
