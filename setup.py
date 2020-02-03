387ae38703689d01915137cf8a05046db678ed54
Grant Miller <gmiller@extron.com>
2020-02-03 13:44:29 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simple_tcp',
+    version='0.0.0',
+    packages=['simple_tcp'],
+    url='https://github.com/GrantGMiller/simple_tcp',
+    license='MIT',
+    author='Grant Miller',
+    author_email='grant@grant-miller.com',
+    description='Simple TCP socket object'
+)
