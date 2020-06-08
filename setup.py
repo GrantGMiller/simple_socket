28bb9696b60b9d207eb2b9fc19998ae41dd687bf
Grant Miller <grant@grant-miller.com>
2020-06-08 17:12:25 -0400

    removed gs_tools dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from setuptools import setup
 
 setup(
     name='simple_socket',
-    version='0.0.1',
+    version='0.0.2',
+    # 0.0.2 - Added readme.md
+    # 0.0.1 - init release
     packages=['simple_socket'],
     url='https://github.com/GrantGMiller/simple_socket',
     license='MIT',
