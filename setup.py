790a874f65c623c038004b2f042983f76fb15946
Grant Miller <grant@grant-miller.com>
2020-06-25 15:55:23 -0400

    Updated README.rst

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
+from os import path
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 setup(
     name='simple_socket',
-    version='0.0.3',
+    version='0.0.5',
+    # 0.0.5 - Added README.rst
     # 0.0.3 - Added SSL server/client
     # 0.0.2 - Added readme.md
     # 0.0.1 - init release
     packages=['simple_socket'],
     url='https://github.com/GrantGMiller/simple_socket',
+    long_description=long_description,
     license='MIT',
     author='Grant Miller',
     author_email='grant@grant-miller.com',
